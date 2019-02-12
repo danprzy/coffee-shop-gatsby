1a4d2d2f6ef64c905726c1546436de3022ef20eb
Danuta Przydrozna <danprzy@gmail.com>
2019-02-12 10:55:41 +0100

    add bootstrap css and react-icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 
 import SEO from "../components/seo";
+import { FaGulp } from "react-icons/fa";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <FaGulp />
   </Layout>
 );
 
